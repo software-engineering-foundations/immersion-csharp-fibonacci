4254550c087ae2e153b4a3fa9ed1018b8b7dd850
Atakan Colak <atakan.colak@softwire.com>
2023-10-17 16:22:13 +0100

    Add recursive method

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -1,8 +1,13 @@
 namespace Fibonacci;
 
 public static class FibonacciCalculator {
-    public static long Calculate(int position) 
+    public static long Calculate(int position, bool isRecursive = false) 
     {
+        if (isRecursive) 
+        {
+            return CalculateRecursive(position);
+        }
+
         if (position <= 2)
         {
             return 1;
@@ -20,5 +25,28 @@ public static class FibonacciCalculator {
         }
 
         return j;
+    }
+
+    private static long CalculateRecursive(int initialPosition, int? position = null, int left = 0, int right = 1) {
+        var currentPosition = position ?? initialPosition;
+
+        if (initialPosition == 0) 
+        {
+            return 0;
+        }
+
+        if (currentPosition == 0)
+        {
+            return left;
+        }
+
+        if (initialPosition > 0) 
+        {
+            return CalculateRecursive(initialPosition, currentPosition - 1, right, left + right);
+        }
+        else 
+        {
+            return CalculateRecursive(initialPosition, currentPosition + 1, right - left, left);
+        }
     }
 }
--- FibonacciTests.cs
+++ FibonacciTests.cs
@@ -14,4 +14,15 @@ public class FibonacciTests
             Fibonacci.FibonacciCalculator.Calculate(i + 1).Should().Be(expectedOutputs[i]);
         }
     }
+
+    [Test]
+    public void TestNegativeRecursiveFibonacci()
+    {
+        var inputs = new int[]{-6, -5, -4, -3, -2, -1, 0};
+        var expectedOutputs = new int[]{-8, 5, -3, 2, -1, 1, 0};
+        for (int i = 0; i < expectedOutputs.Length; i++)
+        {
+            Fibonacci.FibonacciCalculator.Calculate(inputs[i], true).Should().Be(expectedOutputs[i]);
+        }
+    }
 }
