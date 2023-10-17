a23341c3fe47ec545c2ac182b2afd227b9002a23
Atakan Colak <atakan.colak@softwire.com>
2023-10-17 16:26:55 +0100

    Rename variables

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -3,22 +3,22 @@ namespace Fibonacci;
 public static class FibonacciCalculator {
     public static long Calculate(int position) 
     {
-        if (position <= 2)
+        if (position == 1 || position == 2)
         {
             return 1;
         }
 
-        var i = 1;
-        var j = 1;
+        var smallFibonacciNumber = 1;
+        var largeFibonacciNumber = 1;
 
         var currentPosition = 2;
         while(currentPosition < position) {
-            var temp = i;
-            i = j;
-            j += temp;
+            var temp = smallFibonacciNumber;
+            smallFibonacciNumber = largeFibonacciNumber;
+            largeFibonacciNumber += temp;
             currentPosition++;
         }
 
-        return j;
+        return largeFibonacciNumber;
     }
 }
